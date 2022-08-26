6e8d84fae55f6db1c7446a840c6004d0f53f1c9c
Tin Tran <tindl88@gmail.com>
2022-08-26 13:26:01 +0700

    feat: 🎸 Add Toast component (#38)

---
+++ src/core-ui/toast/index.tsx
@@ -0,0 +1,19 @@
+import {useLayoutEffect, useRef} from 'react';
+
+import Toast, {IToast, IToastItem} from './toast';
+
+const useToast = () => {
+  const toastRef = useRef<IToast>();
+
+  const show = ({type, title, content, icon, lifeTime = 3000}: IToastItem) => {
+    toastRef.current?.show({type, title, content, icon, lifeTime});
+  };
+
+  useLayoutEffect(() => {
+    toastRef.current = Toast.getInstance();
+  }, []);
+
+  return {show};
+};
+
+export default useToast;
