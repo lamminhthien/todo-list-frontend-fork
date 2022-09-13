6fe9f25593c43e02df1798a94cfdd484c9922e94
Nguyễn Thị Thanh Tuyền <110363843+GreencrisMessi@users.noreply.github.com>
2022-09-13 14:59:29 +0700

    fix: 🐛 TODO-103 Fix Page Action: Username chưa giống design. (#77)
    Co-authored-by: Thiện Lâm <lamminhthien02012000@gmail.com>

--- src/pages/action/index.tsx
+++ src/pages/action/index.tsx
@@ -63,13 +63,13 @@ export default function Action() {
       <>
         <Seo title={`${siteSettings.name} | Action Page`} description={siteSettings.description} />
         <div className={styles['page-action']}>
+          <div className="user sticky-top">
+            <span className="h5">{userObject.userName}</span>
+            <span className="sep"></span>
+            <span className="h5">My List</span>
+          </div>
           <div className="container">
             <div className="inner">
-              <div className="user">
-                <span className="h5">{userObject.userName}</span>
-                <span className="sep"></span>
-                <span className="h5">My List</span>
-              </div>
               <p className="title">TO-DO LIST</p>
               <p className="h1">Organize your work and life, finally.</p>
               <div className="actions">
