ebc5f2ee691b0335a5969efb5809602b29f90211
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-12 21:09:14 +0700

    💃 Fix Seo Description on page list detail some time not work correctly

--- src/data/ssr/room.ssr.ts
+++ src/data/ssr/room.ssr.ts
@@ -17,46 +17,41 @@ type PageProps = {
 };
 
 export const getStaticProps: GetStaticProps<PageProps, ParsedQueryParams> = async ({locale, params}) => {
-  try {
-    const {id} = params!;
-    let description = '';
-    let title = '';
-    // Get description and 3 task in description
-    await apiTask
-      .getListTasks(id)
-      .then(res => {
-        const listData: ITodo = res.data;
-        const listName = listData.name!;
-        const taskList = listData!.tasks!;
-        const taskFirst = taskList[0] ? `${taskList[0].name}` : '';
-        const taskSecond = taskList[1] ? `- ${taskList[1].name}` : '';
-        const taskThrid = taskList[2] ? `- ${taskList[2].name}` : '';
-        title = listName;
-        const inviteText = `Click this link to join with me and collaborate editor.`;
-        if (taskFirst) description = `${listName}. ${taskFirst} ${taskSecond} ${taskThrid}. ${inviteText} `;
-        else description = `${listName}. ${inviteText}`;
-      })
-      .catch(() => {
-        title = 'List Not Available';
-        description = 'This list Not Available. Check your link or use copy link button to share correct list link.';
-      });
-    return {
-      props: {
-        roomId: id,
-        description,
-        title,
-        ...(await serverSideTranslations(locale!, ['common']))
-      }
-    };
-  } catch (error) {
-    return {
-      notFound: true
-    };
-  }
+  const {id} = params!;
+  let description = '';
+  let title = '';
+  // Get description and 3 task in description
+  await apiTask
+    .getListTasks(id)
+    .then(res => {
+      const listData: ITodo = res.data;
+      const listName = listData.name!;
+      const taskList = listData!.tasks!;
+      const taskFirst = taskList[0] ? `${taskList[0].name}` : '';
+      const taskSecond = taskList[1] ? `- ${taskList[1].name}` : '';
+      const taskThrid = taskList[2] ? `- ${taskList[2].name}` : '';
+      title = listName;
+      const inviteText = `Click this link to join with me and collaborate editor.`;
+      if (taskFirst) description = `${listName}. ${taskFirst} ${taskSecond} ${taskThrid}. ${inviteText} `;
+      else description = `${listName}. ${inviteText}`;
+    })
+    .catch(() => {
+      title = 'List Not Available';
+      description = 'This list Not Available. Check your link or use copy link button to share correct list link.';
+    });
+  return {
+    props: {
+      roomId: id,
+      description,
+      title,
+      ...(await serverSideTranslations(locale!, ['common']))
+    }
+  };
 };
 
 export const getStaticPaths: GetStaticPaths<ParsedQueryParams> = async () => {
   const rooms = await apiTodo.getAllTodo();
   const paths = rooms.data.flatMap((room: {id: string}) => ({params: {id: `${room.id}`}}));
+  // const paths = params;
   return {paths, fallback: 'blocking'};
 };
