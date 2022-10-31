e3c746911d1de47364979f6630422d3dc571a693
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-31 08:56:32 +0700

    now dnd-context lookgood

--- src/components/common/dnd-context/index.tsx
+++ src/components/common/dnd-context/index.tsx
@@ -1,10 +1,20 @@
 import {DndContext, DragEndEvent, UniqueIdentifier} from '@dnd-kit/core';
 import {restrictToVerticalAxis} from '@dnd-kit/modifiers';
-import {FC, useState} from 'react';
+import {arrayMove} from '@dnd-kit/sortable';
+import {useState} from 'react';
 
+import api from '@/data/api';
+import {ITodolistResponse} from '@/data/api/types/list.type';
+import {ITaskResponse} from '@/data/api/types/task.type';
+import {socketUpdateList} from '@/data/socket';
 import {useSensorGroup} from '@/lib/dnd-kit/sensor/sensor-group';
 
-const DNDContext: FC = () => {
+interface IProp {
+  list: ITodolistResponse;
+  setList: (list: ITodolistResponse) => void;
+}
+
+const DNDContext = ({list, setList}: IProp) => {
   const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
 
   const sensor = useSensorGroup();
@@ -13,25 +23,25 @@ const DNDContext: FC = () => {
     setActiveId(null);
     if (!over) return;
     if (active.id !== over.id) {
-      // const taskList: ITaskResponse[] = todoList!.tasks;
-      // const oldIndex = taskList?.findIndex(item => active.id === item.id);
-      // const newIndex = taskList?.findIndex(item => over.id === item.id);
-      // const arrangeTask = arrayMove(todoList!.tasks, oldIndex!, newIndex!);
-      // const newTodoList = {...todoList};
-      // newTodoList.tasks = arrangeTask;
-      // setTodoList(newTodoList as ITodolistResponse);
+      const taskList: ITaskResponse[] = list!.tasks;
+      const oldIndex = taskList?.findIndex(item => active.id === item.id);
+      const newIndex = taskList?.findIndex(item => over.id === item.id);
+      const arrangeTask = arrayMove(list!.tasks, oldIndex!, newIndex!);
+      const newTodoList = {...list};
+      newTodoList.tasks = arrangeTask;
+      setList(newTodoList as ITodolistResponse);
 
-      // arrangeTask.forEach((element, index) => {
-      //   if (element.id === active.id) {
-      //     const taskFirstId = arrangeTask[index - 1]?.id;
-      //     const taskReorderId = arrangeTask[index].id!;
-      //     const taskSecondId = arrangeTask[index + 1]?.id;
-      //     api.task
-      //       .reIndex({taskFirstId, taskReorderId, taskSecondId})
-      //       .then(socketUpdateList)
-      //       .catch(() => {});
-      //   }
-      // });
+      arrangeTask.forEach((element, index) => {
+        if (element.id === active.id) {
+          const taskFirstId = arrangeTask[index - 1]?.id;
+          const taskReorderId = arrangeTask[index].id!;
+          const taskSecondId = arrangeTask[index + 1]?.id;
+          api.task
+            .reIndex({taskFirstId, taskReorderId, taskSecondId})
+            .then(socketUpdateList)
+            .catch(() => {});
+        }
+      });
 
       return {over_id: over.id, active_id: active.id, active: activeId};
     }
