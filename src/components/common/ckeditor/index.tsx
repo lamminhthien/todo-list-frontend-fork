44898fb9b609c4db9e36d21e688cca1fcc0493e8
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-08 10:55:44 +0700

    upload image but don't have async await

--- src/components/common/ckeditor/index.tsx
+++ src/components/common/ckeditor/index.tsx
@@ -1,3 +1,5 @@
+import aws from 'aws-sdk';
+import {ManagedUpload, PutObjectRequest} from 'aws-sdk/clients/s3';
 import React, {FC, useEffect, useRef, useState} from 'react';
 
 import style from './style.module.scss';
@@ -12,15 +14,37 @@ const Editor: FC<IEditorProps> = ({onChange, name, value}) => {
   // const API_URL = '/api';
   // const UPLOAD_ENDPOINT = '/upload-image';
 
+  aws.config.update({
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+    region: process.env.NEXT_PUBLIC_AWS_REGION
+  });
+
+  const s3 = new aws.S3();
+
   const uploadAdapter = (loader: any) => {
     return {
       upload: () => {
-        return new Promise((resolve: any, reject: any) => {
-          const body = new FormData();
-          loader.file.then((file: any) => {
-            body.append('uploading', file);
-            console.log(body);
+        return loader.file.then((file: any) => {
+          const fileName = Date.now();
+
+          const s3ObjectRequest: PutObjectRequest = {
+            Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME!,
+            Body: file,
+            Key: `data1/${fileName}.png`,
+            ACL: 'public-read'
+          };
+
+          let link = ``;
+
+          s3.upload(s3ObjectRequest, (err: Error, response: ManagedUpload.SendData) => {
+            if (err) return 'ERR';
+            if (response) {
+              link = response.Location;
+            }
           });
+          console.log(link);
+          return {default: link};
         });
       }
     };
