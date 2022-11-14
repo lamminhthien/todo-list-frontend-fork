10041426f3a2022deecd047a47f4a3bcd20f2d9d
Xuân Huy <itnxhuy1806@gmail.com>
2022-11-14 12:15:54 +0700

    Update Edit Name & StoryPoint

---
+++ src/components/common/input-autosize/index.tsx
@@ -0,0 +1,56 @@
+import {TextareaAutosize} from '@mui/material';
+import classNames from 'classnames';
+import {FC, FocusEvent, KeyboardEvent} from 'react';
+import {useForm} from 'react-hook-form';
+
+import {IBaseProps} from '@/types';
+
+import style from './style.module.scss';
+
+export interface IInputAutosizeInputs {
+  text: string;
+}
+
+export interface IProps extends IBaseProps {
+  value: string;
+  handleSave: (text: string) => void;
+  placeholder?: string;
+  write?: boolean;
+  navive?: boolean;
+}
+
+const InputAutosize: FC<IProps> = ({className, value, handleSave, write, navive, placeholder}) => {
+  const {register, setValue} = useForm<IInputAutosizeInputs>({defaultValues: {text: value}});
+
+  const onSave = (text: string) => {
+    if (text !== value) handleSave(text);
+  };
+
+  const onBlur = (e: FocusEvent<HTMLTextAreaElement, Element>) => {
+    onSave(e.currentTarget.value as string);
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Escape') {
+      setValue('text', value);
+      e.currentTarget.blur();
+    }
+    if (e.key === 'Enter') {
+      e.currentTarget.blur();
+      onSave(e.currentTarget.value as string);
+    }
+  };
+
+  return (
+    <TextareaAutosize
+      className={classNames(className, navive ? '' : style['default-css'])}
+      {...register('text', {value: value})}
+      placeholder={placeholder}
+      onBlur={onBlur}
+      onKeyDown={onKeyDown}
+      readOnly={!write}
+    />
+  );
+};
+
+export default InputAutosize;
--- src/components/task-detail/task-toolbar/left/index.tsx
+++ src/components/task-detail/task-toolbar/left/index.tsx
@@ -1,52 +1,21 @@
-import {FC, useState} from 'react';
-import {useForm} from 'react-hook-form';
+import {FC} from 'react';
 
-import Icon from '@/core-ui/icon';
+import InputAutosize from '@/components/common/input-autosize';
 import api from '@/data/api';
 import {IBaseProps} from '@/types';
 
 import useTask from '../../hooks/use-task';
 
-interface IFormInputs {
-  name: string;
-}
-
 const Left: FC<IBaseProps> = ({className}) => {
   const {task, write, update} = useTask();
-  const {id, name} = task;
-  const {handleSubmit, register} = useForm<IFormInputs>({mode: 'onChange', defaultValues: {name}});
-  const [isEditing, setIsEditing] = useState(false);
 
-  const onClick = () => {
-    if (write) setIsEditing(true);
+  const handleSave = (text: string) => {
+    api.task.update({id: task.id, name: text}).then(update);
   };
-
-  const onClose = () => {
-    if (write) setIsEditing(false);
-  };
-
-  const onSubmit = handleSubmit(formData => {
-    api.task.update({id, name: formData.name}).then(update);
-    onClose();
-  });
 
   return (
     <div className={className}>
-      {isEditing ? (
-        <form className="name-form" onSubmit={onSubmit}>
-          <input className="name-input" {...register('name', {value: name})} />
-          <button className="h-5" type="submit">
-            <Icon name="ico-check-circle" size={20} className="font-black text-green-600" />
-          </button>
-          <button className="h-6" type="button" onClick={onClose}>
-            <Icon name="ico-x" size={24} className="font-black text-red-600" />
-          </button>
-        </form>
-      ) : (
-        <h2 className="text-h2" onClick={onClick}>
-          {name}
-        </h2>
-      )}
+      <InputAutosize {...{handleSave, value: task.name, write}} />
     </div>
   );
 };
