4ccf97fe705d9ae21006b1fc12169412117c4b57
Xuân Huy <itnxhuy1806@gmail.com>
2022-11-15 13:31:55 +0700

    Create UI prototype

---
+++ src/components/kanban/index.tsx
@@ -0,0 +1,40 @@
+import {FC, useEffect} from 'react';
+
+import api from '@/data/api';
+import {IKanbanColumn} from '@/states/kanban/types';
+
+import useKanban from './hooks';
+
+const Kanban: FC = () => {
+  const {columns, setColumns} = useKanban();
+
+  useEffect(() => {
+    api.todolist.getOne({id: '1hjzi'}).then(res => {
+      if (res) setColumns(res.data.status as IKanbanColumn[]);
+    });
+  }, []);
+
+  return (
+    <div className="grid auto-cols-fr grid-flow-col">
+      {columns.length > 0 &&
+        columns.map(status => {
+          return (
+            <div key={status.id}>
+              <div className="border">{status.name}</div>
+              <div>
+                {status.tasks.map(task => {
+                  return (
+                    <div key={task.id} className="border">
+                      {task.name}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Kanban;
--- src/states/kanban/slice.ts
+++ src/states/kanban/slice.ts
@@ -3,14 +3,14 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {IInitialState, IKanbanColumn} from './types';
 
 const initialState: IInitialState = {
-  columns: []
+  columns: [] as IKanbanColumn[]
 };
 
 export const kanbanSlice = createSlice({
   name: 'kanban',
   initialState,
   reducers: {
-    setItems: (state, action: PayloadAction<IKanbanColumn[]>) => {
+    setColumns: (state, action: PayloadAction<IKanbanColumn[]>) => {
       const {payload} = action;
       state.columns = payload;
     }
