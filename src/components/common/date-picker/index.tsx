246a6f0ded8968c4a919e9ee718b7862f1b5635d
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-22 12:28:27 +0700

    🐛 Improve Date Picker UI Logic, Fix Table render in description and comment form (#277)

--- src/components/common/date-picker/index.tsx
+++ src/components/common/date-picker/index.tsx
@@ -1,7 +1,7 @@
 import TextField from '@mui/material/TextField';
 import {DateTimePicker} from '@mui/x-date-pickers';
 import dayjs, {Dayjs} from 'dayjs';
-import {useState} from 'react';
+import {FocusEvent, useState} from 'react';
 
 import style from './styles.module.scss';
 
@@ -14,30 +14,49 @@ interface IDatePickerProp {
 }
 
 const DatePicker = ({value, onChange, readonly, title, minDateTime}: IDatePickerProp) => {
+  const inputFormat = 'MM/DD/YYYY HH:MM';
   const [day, setDay] = useState<Dayjs | null>(dayjs(value));
+  const [active, setActive] = useState(false);
   const handleChange = (newDay: Dayjs | null) => {
     setDay(newDay);
+  };
+  const onBlur = (e: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>) => {
+    if (!e.relatedTarget) {
+      setActive(false);
+    }
+  };
+  const onFocus = () => {
+    setActive(true);
+  };
+  const onClose = () => {
+    setActive(false);
+  };
+  const placeholderChange = () => {
+    return active ? 'MM/DD/YYYY' : 'None';
   };
 
   return (
     <div className={style['date-time-picker']}>
       <DateTimePicker
-        className="date-input"
-        inputFormat={'MM/DD/YYYY HH:MM'}
+        className={`date-input ${active && 'active'}`}
+        inputFormat={inputFormat}
         showToolbar={true}
         value={day}
         toolbarTitle={title}
         readOnly={readonly}
         minDateTime={dayjs(minDateTime || '14/11/1990 14:11')}
         onChange={handleChange}
+        onClose={onClose}
         onAccept={() => onChange(day?.toDate())}
         renderInput={params => (
           <TextField
             focused={false}
+            onBlur={onBlur}
+            onFocus={onFocus}
             {...params}
             inputProps={{
               ...params.inputProps,
-              placeholder: 'None'
+              placeholder: placeholderChange()
             }}
           />
         )}
