62141f039cda3ce94ca68d853642b86e9356fd3d
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-23 12:07:58 +0700

    🐛 Thiện - Fix and improve Assignee Logic and UI (#280)
    * show assign to me (option)
    
    * add unasigned to me option
    
    * add unassigned
    
    * thêm tích xanh cho người đang được assigne
    
    * alway show list asignee

--- src/components/task-detail/task-body/body-right/assignee/index.tsx
+++ src/components/task-detail/task-body/body-right/assignee/index.tsx
@@ -1,9 +1,11 @@
+import {arrayMove} from '@dnd-kit/sortable';
 import {Autocomplete, Box, TextField} from '@mui/material';
 import classNames from 'classnames';
 import {FC, FocusEvent, useState} from 'react';
 
 import AssigneeIcon from '@/components/common/assignee-icon';
 import api from '@/data/api';
+import {useStateAuth} from '@/states/auth';
 import useTask from '@/states/task/use-task';
 import {IBaseProps} from '@/types';
 import {JoinerBgColos} from '@/utils/constant';
@@ -11,10 +13,18 @@ import {JoinerBgColos} from '@/utils/constant';
 import Title from '../../title';
 
 const Assignee: FC<IBaseProps> = ({className}) => {
+  const auth = useStateAuth();
   const {task, update} = useTask();
   const {id, assignees} = task;
   const options = task.todolist.members.filter(e => e.isActive).map(e => e.user);
+
   const assignee = assignees.filter(e => e.isActive)[0];
+  if (assignee)
+    options.unshift({
+      email: 'null',
+      name: 'UnAssigned',
+      id: 'unassigned'
+    });
   const idOptions = options.map(e => e.id);
   const bg = assignee ? JoinerBgColos[(idOptions.indexOf(assignee.userId) + 1) % JoinerBgColos.length] : undefined;
   const [isEdting, setEditing] = useState(false);
@@ -27,8 +37,16 @@ const Assignee: FC<IBaseProps> = ({className}) => {
     if (email.length > 5) {
       api.task.update({id, assignee: {add: [email]}}).then(update);
     }
+    if (email === 'null') {
+      api.task.update({id, assignee: {remove: ['']}}).then(update);
+    }
   };
 
+  const optionAssignToMe = () => {
+    if (options.length == 0) return [];
+    const assignToMeIndex = options.findIndex(e => e.email == auth?.email);
+    return arrayMove(options, assignToMeIndex, 1);
+  };
   return (
     <div className={classNames('assignee', className)}>
       <Title text="Assignee" />
@@ -36,18 +54,19 @@ const Assignee: FC<IBaseProps> = ({className}) => {
         <Autocomplete
           disablePortal
           freeSolo
-          options={options}
+          options={optionAssignToMe()}
           getOptionLabel={option => (option as any).email}
           onBlur={onClose}
+          open={true}
           renderInput={params => {
             return <TextField {...params} label="User" onBlur={onBlur} autoFocus className="ring-0" />;
           }}
           renderOption={(props, option) => {
             return (
               <Box component="li" {...props}>
-                {option.email}
                 <br />
-                {option.name}
+                {option.email?.includes(auth?.email || '  ') ? `${option.name} (Assign to me)` : option.name}
+                {assignee?.user.email === option.email && '✅'}
               </Box>
             );
           }}
