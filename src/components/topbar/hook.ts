388bfe3ee3878dee0e03bd6a610d58d02d5552ef
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-06 16:25:49 +0700

    🎸 TODOOY-11 Thêm trạng thái active underline cho My task/My list (#334)
    * 🎸 refactor topbar hooks
    
    * 🎸 set active border on my tasks or my lists in topbar
    
    * init style for active my task or my list

---
+++ src/components/topbar/hook.ts
@@ -0,0 +1,43 @@
+import {useRouter} from 'next/router';
+import {useState} from 'react';
+
+import {ROUTES} from '@/configs/routes.config';
+import {useStateAuth} from '@/states/auth';
+import LocalStorage from '@/utils/local-storage';
+
+const useTopbar = () => {
+  const router = useRouter();
+  const auth = useStateAuth();
+  const [socialOpen, setSocialOpen] = useState(false);
+  const handleSocial = () => setSocialOpen(true);
+  const currentPage = router.pathname;
+
+  const returnTo = (curPage: string) => {
+    const checkPage = LocalStorage.checkPage.get();
+    switch (curPage) {
+      case `${ROUTES.LIST}`:
+        router.push(ROUTES.HOME);
+        break;
+      case `${ROUTES.TASK}`:
+        router.push(ROUTES.LIST);
+        break;
+      case `${ROUTES.LIST}/[id]`:
+        if (checkPage === '/tasks') router.push(ROUTES.TASK);
+        else router.push(ROUTES.LIST);
+        break;
+      case `${ROUTES.TASK}/[id]`:
+        if (checkPage === '/lists') {
+          router.push(ROUTES.LIST + '/' + LocalStorage.listId.get());
+        } else if (checkPage === '/tasks') {
+          router.push(ROUTES.TASK);
+        }
+        break;
+      default:
+        router.back();
+    }
+  };
+
+  return {returnTo, currentPage, ROUTES, auth, socialOpen, handleSocial, router, setSocialOpen};
+};
+
+export default useTopbar;
