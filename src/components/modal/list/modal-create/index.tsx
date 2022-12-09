e6a38380995c694c3766ed5022e53c180716ceb5
phuocvuive09 <a000014@abcsoftwarecompany.com>
2022-12-09 08:51:40 +0700

    Revert "TODOOY-30: Fix autofocus for modal"
    This reverts commit 8d305e84717dc492718229597834eb96bb9a9d5e.

--- src/components/modal/list/modal-create/index.tsx
+++ src/components/modal/list/modal-create/index.tsx
@@ -1,10 +1,9 @@
 import cls from 'classnames';
-import {FC, useEffect} from 'react';
+import {FC} from 'react';
 
 import Button from '@/core-ui/button';
 import Input from '@/core-ui/input';
 import {Modal} from '@/core-ui/modal';
-import iosAutoFocus from '@/utils/ios-autofocus';
 
 import styles from '../style-create-upate.module.scss';
 import {IProps} from '../types-create-update';
@@ -12,12 +11,7 @@ import useModalCreateList from './hook';
 
 const ModalCreateList: FC<IProps> = props => {
   const {open, onClose} = props;
-  const {isSubmitting, errors, onSubmit, register, setFocus} = useModalCreateList(props);
-
-  useEffect(() => {
-    setFocus('name');
-    iosAutoFocus();
-  }, [setFocus]);
+  const {isSubmitting, errors, onSubmit, register} = useModalCreateList(props);
 
   return (
     <>
@@ -33,12 +27,7 @@ const ModalCreateList: FC<IProps> = props => {
               <h3 className="title">Create New List</h3>
             </Modal.Header>
             <Modal.Body>
-              <Input
-                error={errors.name?.message}
-                autoFocus={true}
-                placeholder={'Enter your list name'}
-                {...register('name')}
-              />
+              <Input error={errors.name?.message} placeholder={'Enter your list name'} {...register('name')} />
             </Modal.Body>
             <Modal.Footer>
               <div className="content">
