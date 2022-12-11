b58fa87abad2cb3cf55b8a9d5c39d8c1ac24303a
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-12 05:33:26 +0700

    Change description in headline text

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -19,13 +19,26 @@ export const JoinerBgColos = [
 
 export const IndexStep: number = Math.pow(2, 30);
 export const Priorities = {lowest: 'Lowest', low: 'Low', medium: 'Medium', high: 'High', highest: 'Highest'};
-export const PriorityColors = {lowest: '#78716C', low: '#8B5CF6', medium: '#22C55E', high: '#F97316', highest: '#EF4444'};
-export const PriorityIcons = {lowest: 'ico-chevrons-down', low: 'ico-chevron-down', medium: 'ico-equal', high: 'ico-chevron-up', highest: 'ico-chevrons-up'};
+export const PriorityColors = {
+  lowest: '#78716C',
+  low: '#8B5CF6',
+  medium: '#22C55E',
+  high: '#F97316',
+  highest: '#EF4444'
+};
+export const PriorityIcons = {
+  lowest: 'ico-chevrons-down',
+  low: 'ico-chevron-down',
+  medium: 'ico-equal',
+  high: 'ico-chevron-up',
+  highest: 'ico-chevrons-up'
+};
 
 export const LobbyTexts = {
   TITLE: 'TO-DO LIST',
   HEADLINE: 'Make Estimating Agile Projects Accurate & Fun',
-  QUOTE: "Organize your life and complete tasks more reliably by using the right to-do app. These are the top performers we've tested.",
+  QUOTE:
+    "Organize your life easier and complete tasks more reliably by using the right to-do app. These are the top performers we've tested.",
   CREATE: ' Create New List ',
   MY_LISTS: 'My Lists'
 };
