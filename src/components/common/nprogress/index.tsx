657a4449372d5c88ca04a4ec683a7bf81a978ead
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-12 14:17:40 +0700

    🐛 Restore to old method

--- src/components/common/nprogress/index.tsx
+++ src/components/common/nprogress/index.tsx
@@ -1,17 +1,12 @@
 import {useRouter} from 'next/router';
 import nProgress from 'nprogress';
 import {ReactNode, useEffect} from 'react';
-import useSWR from 'swr';
 
 interface INProgresProps {
   children: ReactNode;
 }
 
-const fetcher = (url: RequestInfo | URL) => fetch(url).then(res => res.json());
-const apiRoute = `${process.env.NEXT_PUBLIC_SITE_URL}/api/server-build-id`;
-
 const NProgres = ({children}: INProgresProps) => {
-  const {data} = useSWR(`${apiRoute}`, fetcher);
   const router = useRouter();
 
   nProgress.configure({
@@ -21,40 +16,22 @@ const NProgres = ({children}: INProgresProps) => {
     showSpinner: true
   });
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    const serverBuildID = data?.serverBuildID || 'serverID';
-    console.log('🚀 ~ file: index.tsx:27 ~ useEffect ~ serverBuildID', serverBuildID);
-    const clientBuildID = process.env.NEXT_PUBLIC_GIT_COMMIT_SHA || 'clientID';
-    console.log('🚀 ~ file: index.tsx:29 ~ useEffect ~ clientBuildID', clientBuildID);
-
     const progress = () => {
       nProgress.start();
-      console.log(
-        '🚀 ~ file: index.tsx:32 ~ progress ~ serverBuildID !== clientBuildID && data && !error',
-        serverBuildID !== clientBuildID && data !== undefined
-      );
-      if (serverBuildID !== clientBuildID && data !== undefined) {
-        console.log('Deploy Reload');
-        router.reload();
-      }
     };
-
     const doneProgress = () => {
       nProgress.done();
     };
-
     router.events.on('routeChangeStart', progress);
     router.events.on('routeChangeComplete', doneProgress);
     router.events.on('routeChangeError', doneProgress);
-
     return () => {
       router.events.off('routeChangeStart', progress);
       router.events.off('routeChangeComplete', doneProgress);
       router.events.off('routeChangeError', doneProgress);
     };
   }, []);
-
   return <>{children}</>;
 };
 
