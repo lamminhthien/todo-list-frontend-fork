9ccd2b6bad506929d04df4efcb5ece2b41beeea6
Phuoc Huu <110363738+phuocvuive09@users.noreply.github.com>
2023-01-03 08:53:08 +0700

    NOTIFICATION-8: Fix date in notification (#409)
    * NOTIFICATION-8: Fix date in notification
    
    * Format name of function

--- src/utils/date-format/notification.ts
+++ src/utils/date-format/notification.ts
@@ -1,8 +1,10 @@
+import {formatAmPm} from './format-am-pm';
+
 export const formatForNotification = (dateString: string) => {
   const date = new Date(dateString);
   const month = date.toLocaleString('default', {month: 'long'});
   const day = date.getDate();
-  const hour = date.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
+  const hour = formatAmPm(date);
 
   const result = `${month} ${day} at ${hour}`;
   return result;
