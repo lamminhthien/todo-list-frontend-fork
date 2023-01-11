c244ce373cedf1ae6d85b02747393b9a4b7bba26
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-11 13:44:44 +0700

    TODOOY-56 Bring switch mode list button to navigation bar (#431)
    * fix scroll bar horizontal a bit
    
    * bring menu switch list mode to navigation bar

--- src/components/topbar-new/navigation/index.tsx
+++ src/components/topbar-new/navigation/index.tsx
@@ -3,12 +3,19 @@ import {useRouter} from 'next/router';
 import {FC} from 'react';
 
 import {ROUTES} from '@/configs/routes.config';
+import Icon from '@/core-ui/icon';
+import {isBoardPage, isListDetailPage} from '@/utils/check-routes';
 
 import style from './style.module.scss';
 
 const Navigation: FC = () => {
   const router = useRouter();
   const currentPage = router.asPath;
+  const {id} = router.query;
+
+  const isKanbanView = isBoardPage(currentPage, id as string);
+  const isListView = isListDetailPage(currentPage, id as string);
+
   return (
     <div className={style.navigation}>
       <div className={style['menu-bar']}>
@@ -20,11 +27,31 @@ const Navigation: FC = () => {
             <a className={(currentPage === ROUTES.LIST && style.active) || ''}>My Lists</a>
           </Link>
         </div>
-        {/* <div className={style['menu-bar-right']}>
-          <div className="search-box">
+        <div className={style['menu-bar-right']}>
+          {/* <div className="search-box">
             <input placeholder="Search" />
-          </div>
-        </div> */}
+          </div> */}
+          {(isKanbanView || isListView) && (
+            <>
+              <div className={`kanban-view ${isKanbanView && style['active-list-mode']}`}>
+                <Icon
+                  name="list-view"
+                  className="ico-vertical leading-tight text-[#475569] hover:cursor-pointer"
+                  size={20}
+                  onClick={() => router.push(`${ROUTES.KANBAN}/${id}`)}
+                />
+              </div>
+              <div className={`list-view ${isListView && style['active-list-mode']}`}>
+                <Icon
+                  name="horizontal"
+                  className="ico-horizontal leading-tight text-[#475569] hover:cursor-pointer"
+                  size={20}
+                  onClick={() => router.push(`${ROUTES.LIST}/${id}`)}
+                />
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
