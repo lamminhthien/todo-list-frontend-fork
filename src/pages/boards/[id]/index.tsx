7c97b89699681d50680fe7a0ff4ef67cc46826ae
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-11 16:38:57 +0700

    TODOOY-57: Unlock Board page for production (#432)

--- src/pages/boards/[id]/index.tsx
+++ src/pages/boards/[id]/index.tsx
@@ -2,7 +2,6 @@ import {InferGetStaticPropsType} from 'next';
 import React from 'react';
 
 import KanbanDetail from '@/components/board';
-import ErrorInformation from '@/components/common/404';
 import Seo from '@/components/common/seo/seo';
 import ToolBar from '@/components/toolbar-list-detail';
 import {getStaticPaths, getStaticProps} from '@/data/ssr/lists.ssr';
@@ -11,16 +10,13 @@ import NewLayout from '@/layouts/new-layout';
 export {getStaticPaths, getStaticProps};
 
 export default function PageListDetailKanban({id, seo}: InferGetStaticPropsType<typeof getStaticProps>) {
-  const publicSiteURL = process.env.NEXT_PUBLIC_SITE_URL;
-  if (publicSiteURL?.includes('localhost') || publicSiteURL?.includes('stage'))
-    return (
-      <>
-        <Seo {...seo} />
-        <ToolBar />
-        <KanbanDetail id={id} />
-      </>
-    );
-  return <ErrorInformation />;
+  return (
+    <>
+      <Seo {...seo} />
+      <ToolBar />
+      <KanbanDetail id={id} />
+    </>
+  );
 }
 
 PageListDetailKanban.Layout = NewLayout;
