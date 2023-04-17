aa242bc28437077f65830107b6420d841c7a043f
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-04-17 08:17:58 +0700

    TURN OFF DRAG COLUMN (#460)

--- src/components/board/column/wrapper/index.tsx
+++ src/components/board/column/wrapper/index.tsx
@@ -1,5 +1,3 @@
-import {useSortable} from '@dnd-kit/sortable';
-import {CSS} from '@dnd-kit/utilities';
 import React, {FC, ReactNode} from 'react';
 
 interface IKanbanColumnWrapperProps {
@@ -7,15 +5,8 @@ interface IKanbanColumnWrapperProps {
   id: string;
 }
 
-const KanbanColumnWrapper: FC<IKanbanColumnWrapperProps> = ({id, children}) => {
-  const {attributes, listeners, setNodeRef, transform, transition, isDragging} = useSortable({id});
-  const styleDnd = {transform: CSS.Transform.toString(transform), transition, opacity: isDragging ? 0.5 : 1};
-
-  return (
-    <ul className="kanban-column-container h-full" ref={setNodeRef} style={styleDnd} {...attributes} {...listeners}>
-      {children}
-    </ul>
-  );
+const KanbanColumnWrapper: FC<IKanbanColumnWrapperProps> = ({children}) => {
+  return <ul className="kanban-column-container h-full">{children}</ul>;
 };
 
 export default KanbanColumnWrapper;
