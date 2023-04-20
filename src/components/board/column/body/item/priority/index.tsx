d61f3ecbbdc9ac99bce5f613da82563a0ac83cf6
Huy Ngo <itnxhuy1806@gmail.com>
2023-04-20 12:00:14 +0700

    Update handle index

--- src/components/board/column/body/item/priority/index.tsx
+++ src/components/board/column/body/item/priority/index.tsx
@@ -6,17 +6,16 @@ import {PriorityColors, PriorityIcons} from '@/utils/constant';
 import style from './style.module.scss';
 
 interface IKanbanTaskPriorityProps {
-  priority: keyof typeof PriorityIcons;
+  priority: string;
 }
 
 const KanbanTaskPriority: FC<IKanbanTaskPriorityProps> = ({priority}) => {
+  const key = priority.toLowerCase() as keyof typeof PriorityIcons; //FIXME: priority = medium
+  const iconName = PriorityIcons[key];
+
   return (
     <div className={style['kanban-task-priority']}>
-      <div className="h-[30px] w-[30px]">
-        <div className={`${style.inner}`}>
-          <Icon name={PriorityIcons[priority]} style={{color: PriorityColors[priority]}} />
-        </div>
-      </div>
+      <Icon name={iconName} style={{color: PriorityColors[key]}} />
     </div>
   );
 };
--- src/components/board/container/index.tsx
+++ src/components/board/container/index.tsx
@@ -3,6 +3,7 @@ import {arrayMove, horizontalListSortingStrategy, SortableContext} from '@dnd-ki
 import React, {FC, useEffect, useState} from 'react';
 
 import api from '@/data/api';
+import {socketUpdateList} from '@/data/socket';
 import {useBoardState} from '@/hooks/useBoardState';
 import {useSensorGroup} from '@/lib/dnd-kit/sensor/sensor-group';
 import useBoards from '@/states/board/use-boards';
@@ -22,29 +23,33 @@ const KanbanContainer: FC = () => {
   const [boardState, setBoardState] = useState<BoardState>({ids: [], entities: {}});
   const [activeItemId, setActiveItemId] = useState<string>();
   const [activeColumnId, setActiveColumnId] = useState<string>();
+  const [needUpdate, setNeedUpdate] = useState(false);
 
   useEffect(() => {
     boardStore.generateState(statusList);
+    setNeedUpdate(true);
   }, [statusList]);
 
   useEffect(() => {
-    if (!boardState.ids.length) {
+    if (needUpdate) {
       const entities: BoardState['entities'] = {};
       const ids = boardStore.ids.map(key => {
         entities[key] = boardStore.entitiesColumn[key].ids;
         return key;
       });
       setBoardState({ids, entities});
+      setNeedUpdate(false);
     }
-  }, [boardStore]);
+  }, [needUpdate, boardStore]);
 
   const getDragData = ({active, over}: {active: Active; over: Over | null}) => {
+    console.log('🚀 ~ file: index.tsx:46 ~ getDragData ~ over:', over);
     if (!over?.id) return null;
     const activeId = active.id as string;
     const activeColumn = active.data.current?.sortable?.containerId as string | undefined;
     const overId = over.id as string;
     const overContainerId = over?.data.current?.sortable?.containerId as string | undefined;
-    const overColumn = activeColumn !== 'board' && overContainerId === 'board' ? overId : overContainerId;
+    const overColumn = activeColumn !== 'board' ? (overContainerId === 'board' ? overId : overContainerId) : 'board';
     if (!activeColumn || !overColumn) return null;
     const activeIndex = active?.data.current?.sortable?.index as number | undefined;
     const overIndex = over?.data.current?.sortable?.index as number | undefined;
@@ -80,8 +85,10 @@ const KanbanContainer: FC = () => {
     const {activeColumn, overColumn, activeIndex, overIndex, activeId} = dragData;
     //move column
     if (activeColumn === 'board' && overColumn === 'board') {
+      const indexList = boardState.ids
+        .filter(e => e !== activeId)
+        .map(e => Number(boardStore.entitiesColumn[e].status.index));
       const newIds = arrayMove(boardState.ids, activeIndex, overIndex);
-      const indexList = boardState.ids.map(e => Number(boardStore.entitiesColumn[e].status.index));
       setBoardState({...boardState, ids: newIds});
       const prevIndex = boardStore.entitiesColumn[newIds[overIndex - 1]]?.status.index;
       const nextIndex = boardStore.entitiesColumn[newIds[overIndex + 1]]?.status.index;
@@ -90,14 +97,18 @@ const KanbanContainer: FC = () => {
         boardStore.updateState(state => {
           state.entitiesColumn[activeId].status.index = statusIndex;
         });
-        api.todolist.update({id: listID, statusId: Number(activeId), statusIndex, resetIndexStatus});
+        api.todolist
+          .update({id: listID, statusId: Number(activeId), statusIndex, resetIndexStatus})
+          .then(socketUpdateList);
       }
     }
     // move item
     else if (activeColumn === overColumn) {
       const newEntities = {...boardState.entities};
+      const indexList = boardState.entities[overColumn]
+        .filter(e => e !== activeId)
+        .map(e => Number(boardStore.entitiesItem[e].indexColumn));
       newEntities[activeColumn] = arrayMove(newEntities[activeColumn], activeIndex, overIndex);
-      const indexList = boardState.entities[overColumn].map(e => Number(boardStore.entitiesItem[e].indexColumn));
       setBoardState({...boardState, entities: newEntities});
       const prevIndex = boardStore.entitiesItem[newEntities[overColumn][overIndex - 1]]?.indexColumn;
       const nextIndex = boardStore.entitiesItem[newEntities[overColumn][overIndex + 1]]?.indexColumn;
@@ -106,7 +117,9 @@ const KanbanContainer: FC = () => {
         boardStore.updateState(state => {
           state.entitiesItem[activeId].indexColumn = indexColumn;
         });
-        api.task.update({id: activeId, statusId: Number(overColumn), indexColumn, resetIndexColumn});
+        api.task
+          .update({id: activeId, statusId: Number(overColumn), indexColumn, resetIndexColumn})
+          .then(socketUpdateList);
       }
     }
   };
