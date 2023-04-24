d3a0a92053c62c7c1b7f28bc9ee596250b3e0f33
nyansilence <47050063+nyansilence@users.noreply.github.com>
2023-04-24 15:53:01 +0700

    TD-321 Fix lại UI của list và KANBAN cho icon to lên 20 và căn giữa (#484)
    * TD-321 Fix lại UI của list và KANBAN cho icon to lên 20 và căn giữa text
    
    * fix hover reset

--- src/components/toolbar-list-detail/toolbar-right/index.tsx
+++ src/components/toolbar-list-detail/toolbar-right/index.tsx
@@ -51,31 +51,31 @@ const ToolBarRight: FC = () => {
     <div className={style['toolbar-right']}>
       <div className="view-mode">
         {!isKanbanView && (write || owner) ? (
-          <div className={`add-task hover:cursor-pointer`} onClick={onAddTask}>
+          <div className={`add-task flex items-center hover:cursor-pointer`} onClick={onAddTask}>
             <span className="hidden sm:block">Add Task</span>
-            <Icon name="add-task" className="ico-plus-circle icons" size={16} onClick={onAddTask} />
+            <Icon name="add-task" className="ico-plus-circle icons" size={20} onClick={onAddTask} />
           </div>
         ) : (
           ''
         )}
-        <div className={`kanban-view ${!isKanbanView ? '' : 'active'}`}>
+        <div className={`kanban-view ${!isKanbanView ? '' : 'active'} flex items-center`}>
           <Icon
             name="list-view"
             className="ico-vertical icons"
-            size={16}
+            size={20}
             onClick={() => router.push(`${ROUTES.KANBAN}/${id}`)}
           />
         </div>
-        <div className={`list-view ${isKanbanView ? '' : 'active'}`}>
+        <div className={`list-view ${isKanbanView ? '' : 'active'} flex items-center`}>
           <Icon
             name="horizontal"
             className="ico-horizontal icons"
-            size={16}
+            size={20}
             onClick={() => router.push(`${ROUTES.LIST}/${id}`)}
           />
         </div>
         <div className="flex cursor-pointer items-center" onClick={() => router.push(`${ROUTES.DOCUMENT}/${id}`)}>
-          <Icon name="documents" className="ico-note-list icons" size={24} />
+          <Icon name="documents" className="ico-note-list icons" size={20} />
           <span>Docs</span>
         </div>
         {isKanbanView && (
@@ -96,16 +96,16 @@ const ToolBarRight: FC = () => {
           </div>
         )}
         {(write || owner) && (
-          <div className="delete ml-1 hover:cursor-pointer" onClick={onDelete}>
+          <div className="delete ml-1 flex items-center hover:cursor-pointer" onClick={onDelete}>
+            <Icon name="Delete list" className="ico-trash-2 icons" size={20} onClick={onDelete} />
             <span className="hidden sm:block">Delete</span>
-            <Icon name="Delete list" className="ico-trash-2 icons" size={16} onClick={onDelete} />
           </div>
         )}
       </div>
       {(write || owner) && (
-        <div className="settings hover:cursor-pointer" onClick={() => onSettingBoard()}>
+        <div className="settings flex items-center hover:cursor-pointer" onClick={() => onSettingBoard()}>
+          <Icon name="Settings" className="ico-settings icons" size={20} />
           <span className="hidden sm:block">Settings</span>
-          <Icon name="Settings" className="ico-settings icons" size={16} />
         </div>
       )}
     </div>
