2ef774810d44394e9a9b68558492ae3b7be1a2b9
nyansilence <nyansilence@gmail.com>
2023-04-24 16:18:17 +0700

    TD-319 KANBAN: kiểm tra lại khi nhấn qua giao diện list xong nhấn lại giao diện KANBAN, xong f5 lại mất nút setting và nút delete

--- src/components/toolbar-list-detail/toolbar-right/index.tsx
+++ src/components/toolbar-list-detail/toolbar-right/index.tsx
@@ -16,7 +16,7 @@ const ToolBarRight: FC = () => {
   const path = router.asPath;
   const {id} = router.query;
 
-  const {boardData} = useBoards();
+  const {boardData, owner: boardOwner, write: boardWrite} = useBoards();
   const {todolist, statusList, write, owner} = useTodolist();
   const {setIsOpenModal, setSelectedTodolist, setSelectedColumnId} = useModals();
 
@@ -50,7 +50,7 @@ const ToolBarRight: FC = () => {
   return (
     <div className={style['toolbar-right']}>
       <div className="view-mode">
-        {!isKanbanView && (write || owner) ? (
+        {!isKanbanView && (write || owner || boardOwner || boardWrite) ? (
           <div className={`add-task hover:cursor-pointer`} onClick={onAddTask}>
             <span className="hidden sm:block">Add Task</span>
             <Icon name="add-task" className="ico-plus-circle icons" size={16} onClick={onAddTask} />
@@ -95,14 +95,14 @@ const ToolBarRight: FC = () => {
             <ToolFilter todolist={todolist} />
           </div>
         )}
-        {(write || owner) && (
+        {(write || owner || boardOwner || boardWrite) && (
           <div className="delete ml-1 hover:cursor-pointer" onClick={onDelete}>
             <span className="hidden sm:block">Delete</span>
             <Icon name="Delete list" className="ico-trash-2 icons" size={16} onClick={onDelete} />
           </div>
         )}
       </div>
-      {(write || owner) && (
+      {(write || owner || boardOwner || boardWrite) && (
         <div className="settings hover:cursor-pointer" onClick={() => onSettingBoard()}>
           <span className="hidden sm:block">Settings</span>
           <Icon name="Settings" className="ico-settings icons" size={16} />
