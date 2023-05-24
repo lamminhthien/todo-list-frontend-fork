256150e168883c729921a340ab03a5accd14a130
Thinh <121143484+thinhabcsoftware@users.noreply.github.com>
2023-05-24 12:03:03 +0700

    Add open link in new tab when right-click (#509)

--- src/components/topbar-new/top-area/right/link/index.tsx
+++ src/components/topbar-new/top-area/right/link/index.tsx
@@ -8,14 +8,15 @@ import style from './style.module.scss';
 const TopBarLink = () => {
   const router = useRouter();
   const currentPage = router.pathname;
+
   return (
     <div className={style['topbar-link']}>
       <Link href={ROUTES.TASK}>
-        <span className={(currentPage === ROUTES.TASK && style.active) || ''}>My Tasks</span>
+        <a className={(currentPage === ROUTES.TASK && style.active) || 'un-active'}>My Tasks</a>
       </Link>
       <i className="inline-block font-light not-italic text-gray-200">|</i>
       <Link href={ROUTES.LIST}>
-        <span className={(currentPage === ROUTES.LIST && style.active) || ''}>My Lists</span>
+        <a className={(currentPage === ROUTES.LIST && style.active) || 'un-active'}>My Lists</a>
       </Link>
     </div>
   );
