39b6e7ff4bf55cf07ed5a510cf92be26269cbc7a
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-08-14 15:21:44 +0700

    TD-377-improve: Remove rename on file attachment

--- src/components/task-detail/task-body/body-left/attachment/files/index.tsx
+++ src/components/task-detail/task-body/body-left/attachment/files/index.tsx
@@ -3,15 +3,12 @@ import {ButtonBase, Popover} from '@mui/material';
 import classNames from 'classnames';
 import Link from 'next/link';
 import {FC, MouseEvent, useState} from 'react';
-import {SubmitHandler, useForm} from 'react-hook-form';
 
 import Button from '@/core-ui/button';
 import Icon from '@/core-ui/icon';
 import IconButton from '@/core-ui/icon-button';
-import Input from '@/core-ui/input';
 import useToast from '@/core-ui/toast';
 import api from '@/data/api';
-import {IAttachmentResponse} from '@/data/api/types/task.type';
 import useTask from '@/states/task/use-task';
 import {IBaseProps} from '@/types';
 import {getDate} from '@/utils/get-date';
@@ -19,53 +16,23 @@ import {ToastContents} from '@/utils/toast-content';
 
 import style from './style.module.scss';
 
-interface IFormInputs {
-  name: string;
-}
 const FileAttachment: FC<IBaseProps> = ({className}) => {
   const toast = useToast();
-  const [imageSelected, setImageSelected] = useState<number>();
   const {task, write, update} = useTask();
 
   const attachments = task.attachments.filter(e => e.isActive);
 
-  const {handleSubmit, setValue, setFocus, register} = useForm<IFormInputs>({
-    mode: 'onChange',
-    defaultValues: {name: ''}
-  });
-  const [anchorRenameEl, setAnchorRenameEl] = useState<HTMLButtonElement | null>(null);
   const [anchorDeleteEl, setAnchorDeleteEl] = useState<HTMLButtonElement | null>(null);
-
-  const openName = Boolean(anchorRenameEl);
-  const editButtonId = openName ? 'simple-popover' : undefined;
 
   const openDelete = Boolean(anchorDeleteEl);
   const deleteButtonId = openDelete ? 'simple-popover' : undefined;
 
-  const onClick = (event: MouseEvent<HTMLButtonElement>, {id, name}: IAttachmentResponse, type: string) => {
-    if (type == 'rename') {
-      setAnchorRenameEl(event.currentTarget);
-      setImageSelected(id);
-      setValue('name', name);
-    }
-    if (type == 'delete') setAnchorDeleteEl(event.currentTarget);
-  };
-
-  const onCloseRename = () => {
-    setAnchorRenameEl(null);
+  const onClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorDeleteEl(event.currentTarget);
   };
 
   const onCloseDelete = () => {
     setAnchorDeleteEl(null);
-  };
-
-  const submitHandler: SubmitHandler<IFormInputs> = ({name}) => {
-    if (task && imageSelected)
-      api.task
-        .update({id: task.id, attachment: {update: {id: imageSelected, name}}})
-        .then(update)
-        .catch(() => toast.show({type: 'danger', title: 'Edit Image', content: ToastContents.ERROR}));
-    onCloseRename();
   };
 
   const handleDelete = (imageId: number) => {
@@ -86,42 +53,17 @@ const FileAttachment: FC<IBaseProps> = ({className}) => {
           .filter(item => item?.type && item.type !== 'image')
           .map((e, idx) => (
             <div key={idx} className={classNames('attachment', `${!task ? 'upload' : ''}`)}>
-              <Icon name="ico-file-download" size={48} className="btn-created-date" />
+              <Icon name="ico-file-download" size={48} className="icon-download" />
               {e.createdDate && (
                 <div className="info">
                   <div className="info-name">{e.name}</div>
                   <div className="info-date"> {'Added ' + getDate(new Date(e.createdDate))}</div>
                   {write && (
                     <div className="info-actions">
-                      <ButtonBase aria-describedby={editButtonId} onClick={event => onClick(event, e, 'rename')}>
-                        Rename
-                      </ButtonBase>
-                      <Popover
-                        id={editButtonId}
-                        open={openName}
-                        anchorEl={anchorRenameEl}
-                        onClose={onCloseRename}
-                        onFocus={() => setFocus('name', {shouldSelect: true})}
-                        anchorOrigin={{
-                          vertical: 'bottom',
-                          horizontal: 'left'
-                        }}
-                      >
-                        <form className="relative p-5 text-h7" onSubmit={handleSubmit(submitHandler)}>
-                          <IconButton name="ico-x" className="absolute right-3 top-3" onClick={onCloseRename} />
-                          <div className="border-b pb-4 text-center font-medium text-slate-500">Edit attachment</div>
-                          <div className="mt-3 font-bold text-slate-700">Name</div>
-                          <Input className="my-2 min-w-[300px] p-1" {...register('name', {required: true})} />
-                          <Button color="primary" variant="contained" className="h-8 w-full" type="submit">
-                            Update
-                          </Button>
-                        </form>
-                      </Popover>
-                      <ButtonBase aria-describedby={deleteButtonId} onClick={event => onClick(event, e, 'delete')}>
+                      <ButtonBase aria-describedby={deleteButtonId} onClick={event => onClick(event)}>
                         Delete
                       </ButtonBase>
                       <Popover
-                        id={editButtonId}
                         open={openDelete}
                         anchorEl={anchorDeleteEl}
                         onClose={onCloseDelete}
