cd220c1700b9b9e12409d8fad9c02335ad24e92d
MaiAnhisreal <146809185+MaiAnhisreal@users.noreply.github.com>
2023-12-25 08:54:10 +0700

    Create projects page (#589)

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NewLayout from '@/layouts/new-layout';
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <div>day la trang project</div>
+    </>
+  );
+}
+
+ProjectsPage.Layout = NewLayout;
